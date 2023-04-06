bc959956986a951153e72f44d7fdd9d84d5d1f05
Aimee Laws <aimee.laws13@gmail.com>
2023-04-06 11:44:57 +0100

    added useState for search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   const handleChange = (event) => {
-    console.log(event);
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   }
 
   return (
     <>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" onChange={handleChange} />
+
+      <p>Searching for <strong>{searchTerm}</strong></p>
     </>
   );
 };
