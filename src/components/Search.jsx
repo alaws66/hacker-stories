a746b5cd3ac1cc28cb4ae0e94f652d15e416c6d4
Aimee Laws <aimee.laws13@gmail.com>
2023-04-06 11:26:43 +0100

    added onChange function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,15 @@
-const Search = () => (
-  <>
-    <label htmlFor="search">Search: </label>
-    <input type="text" id="search" />
-  </>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  }
 
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" onChange={handleChange} />
+    </>
+  );
+};
 
 export default Search
