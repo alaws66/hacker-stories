b58872a4e9f2c9043544e6b210e36c7924bd5139
Aimee Laws <aimee.laws13@gmail.com>
2023-04-18 13:59:25 +0100

    layout restructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
-import InputWithLabel from "./components/InputWithLabel";
-import List from "./components/List";
+import * as React from 'react';
+import axios from 'axios';
+import { sortBy } from 'lodash';
 
-import { useEffect, useReducer, useState } from "react";
+const API_BASE = 'https://hn.algolia.com/api/v1';
+const API_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'JavaScript',
-    url: 'https://en.wikipedia.org/wiki/JavaScript',
-    author: 'Brendan Eich',
-    num_comments: 15,
-    points: 10,
-    objectID: 2,
-  },
-];
+const getUrl = (searchTerm, page) =>
+  `${API_BASE}${API_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`;
 
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-  );
+const extractSearchTerm = (url) =>
+  url
+    .substring(url.lastIndexOf('?') + 1, url.lastIndexOf('&'))
+    .replace(PARAM_SEARCH, '');
+
+const getLastSearches = (urls) =>
+  urls
+    .reduce((result, url, index) => {
+      const searchTerm = extractSearchTerm(url);
+
+      if (index === 0) {
+        return result.concat(searchTerm);
+      }
+
+      const previousSearchTerm = result[result.length - 1];
+
+      if (searchTerm === previousSearchTerm) {
+        return result;
+      } else {
+        return result.concat(searchTerm);
+      }
+    }, [])
+    .slice(-6)
+    .slice(0, -1);
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -48,7 +48,11 @@ const storiesReducer = (state, action) => {
         ...state,
         isLoading: false,
         isError: false,
-        data: action.payload,
+        data:
+          action.payload.page === 0
+            ? action.payload.list
+            : state.data.concat(action.payload.list),
+        page: action.payload.page,
       };
     case 'STORIES_FETCH_FAILURE':
       return {
@@ -59,41 +63,62 @@ const storiesReducer = (state, action) => {
     case 'REMOVE_STORY':
       return {
         ...state,
-        data: state.data.filter((story) => action.payload.objectID !== story.objectID),
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
       };
     default:
       throw new Error();
   }
 };
 
-const App = () => {
-  const [searchTerm, setSearchTerm] = useState('React');
-  const [stories, dispatchStories] = useReducer(
-    storiesReducer,
-    { data: [], isLoading: false, isError: false }
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
   );
 
-  useEffect(() => {
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useStorageState(
+    'search',
+    'React'
+  );
+
+  const [urls, setUrls] = React.useState([getUrl(searchTerm, 0)]);
+
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    { data: [], page: 0, isLoading: false, isError: false }
+  );
+
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
-      .then((result) => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
-        });
-      })
-      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-    );
-  }, []);
+    try {
+      const lastUrl = urls[urls.length - 1];
+      const result = await axios.get(lastUrl);
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: {
+          list: result.data.hits,
+          page: result.data.page,
+        },
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
+  }, [urls]);
 
-  const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -102,30 +127,209 @@ const App = () => {
     });
   };
 
+  const handleSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = (searchTerm, page) => {
+    const url = getUrl(searchTerm, page);
+    setUrls(urls.concat(url));
+  };
+
+  const handleSearchSubmit = (event) => {
+    handleSearch(searchTerm, 0);
+
+    event.preventDefault();
+  };
+
+  const handleLastSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+
+    handleSearch(searchTerm, 0);
+  };
+
+  const handleMore = () => {
+    const lastUrl = urls[urls.length - 1];
+    const searchTerm = extractSearchTerm(lastUrl);
+    handleSearch(searchTerm, stories.page + 1);
+  };
+
+  const lastSearches = getLastSearches(urls);
+
   return (
-    <div className="App">
+    <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel
-        id="search"
-        label="Search"
-        value={searchTerm}
-        onInputChange={handleSearch}
-      >
-        <strong>Search: </strong>
-      </InputWithLabel>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
+
+      <LastSearches
+        lastSearches={lastSearches}
+        onLastSearch={handleLastSearch}
+      />
 
       <hr />
 
-      {stories.isError && <p>Something went wrong...</p>}
+      {stories.isError && <p>Something went wrong ...</p>}
+
+      <List list={stories.data} onRemoveItem={handleRemoveStory} />
+
       {stories.isLoading ? (
-        <p>Loading...</p>
+        <p>Loading ...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
-      )} 
+        <button type="button" onClick={handleMore}>
+          More
+        </button>
+      )}
     </div>
   );
-  
 };
 
-export default App
+const LastSearches = ({ lastSearches, onLastSearch }) => (
+  <>
+    {lastSearches.map((searchTerm, index) => (
+      <button
+        key={searchTerm + index}
+        type="button"
+        onClick={() => onLastSearch(searchTerm)}
+      >
+        {searchTerm}
+      </button>
+    ))}
+  </>
+);
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+const SORTS = {
+  NONE: (list) => list,
+  TITLE: (list) => sortBy(list, 'title'),
+  AUTHOR: (list) => sortBy(list, 'author'),
+  COMMENT: (list) => sortBy(list, 'num_comments').reverse(),
+  POINT: (list) => sortBy(list, 'points').reverse(),
+};
+
+const List = ({ list, onRemoveItem }) => {
+  const [sort, setSort] = React.useState({
+    sortKey: 'NONE',
+    isReverse: false,
+  });
+
+  const handleSort = (sortKey) => {
+    const isReverse = sort.sortKey === sortKey && !sort.isReverse;
+
+    setSort({ sortKey, isReverse });
+  };
+
+  const sortFunction = SORTS[sort.sortKey];
+  const sortedList = sort.isReverse
+    ? sortFunction(list).reverse()
+    : sortFunction(list);
+
+  return (
+    <ul>
+      <li style={{ display: 'flex' }}>
+        <span style={{ width: '40%' }}>
+          <button type="button" onClick={() => handleSort('TITLE')}>
+            Title &#8597;
+          </button>
+        </span>
+        <span style={{ width: '20%' }}>
+          <button type="button" onClick={() => handleSort('AUTHOR')}>
+            Author &#8597;
+          </button>
+        </span>
+        <span style={{ width: '20%' }}>
+          <button type="button" onClick={() => handleSort('COMMENT')}>
+            Comments &#8597;
+          </button>
+        </span>
+        <span style={{ width: '15%' }}>
+          <button type="button" onClick={() => handleSort('POINT')}>
+            Points &#8597;
+          </button>
+        </span>
+        <span style={{ width: '5%' }}></span>
+      </li>
+
+      {sortedList.map((item) => (
+        <Item
+          key={item.objectID}
+          item={item}
+          onRemoveItem={onRemoveItem}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const Item = ({ item, onRemoveItem }) => (
+  <li style={{ display: 'flex' }}>
+    <span style={{ width: '40%' }}>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span style={{ width: '20%' }}>{item.author}</span>
+    <span style={{ width: '20%' }}>{item.num_comments}</span>
+    <span style={{ width: '15%' }}>{item.points}</span>
+    <span style={{ width: '5%' }}>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        &#10799;
+      </button>
+    </span>
+  </li>
+);
+
+export default App;
